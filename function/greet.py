733b2680955211cc4ad6ca8248eb380052f469fc
Annetjepkorirr <annetjepkorirr@gmail.com>
2023-04-13 17:12:48 +0300

    first commit

---
+++ function/greet.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"Hello{name}")
+
+def classify(birthdate) :
+    print(f"I was born {birthdate} march")  
---
+++ function/maths.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def divide(q,r):
+    answer= q/r
+    return answer
+
+def subtract(o,w):
+    answer =o-w
+    return answer
+
+def multiply(s,p):
+    answer =s*p
+    return answer
